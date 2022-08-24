203dbad1262f688b4fee1f9966a35f92782e4c31
ahmedraafat050 <ar0506908182@gmail.com>
2022-08-24 10:54:14 +0200

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
     <>
       <div className='hero'>
         <div className="card bg-dark text-white border-0">
-          <img src="/assets/bg.jpg" className="card-img" alt="..." />
+          <img src="/assets/bg.jpg" className="card-img" alt="..."/>
           <div className="card-img-overlay d-flex flex-column justify-content-center">
             <div className="container">
               <h5 className="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVED</h5>
